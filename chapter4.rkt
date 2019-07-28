e53707a5f56732d631f840170ffa4457c23f7f5e
saul <saul.field@gmail.com>
2019-07-28 16:34:22 -0400

    Apply primitive procedures

--- chapter4.rkt
+++ chapter4.rkt
@@ -1,6 +1,55 @@
 ;;; #lang racket
 
-;;; Utility
+;;; run with:
+;;; racket -i -f chapter4.rkt
+
+;;; Utility -------------------------------------------------
+
+(define (true? x)
+  (not (eq? x false)))
+
+(define (false? x)
+  (eq? x false))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (make-procedure params body env)
+  (list 'procedure params body env))
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+(define (procedure-params p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-env p) (cadddr p))
+
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)))
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc)
+        (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+(define (primitive-implementation proc)
+  (cadr proc))
+
+;;; Eval ----------------------------------------------------
 
 (define (self-evaluating? expr)
   (cond ((number? expr) true)
@@ -15,31 +64,10 @@
 (define (text-of-quotation exp)
   (cadr exp))
 
-(define (tagged-list? exp tag)
-  (if (pair? exp)
-      (eq? (car exp) tag)
-      false))
-
-(define (true? x)
-  (not (eq? x false)))
-
-(define (false? x)
-  (eq? x false))
-
-(define (if? exp) (tagged-list? exp 'if))
-(define (if-predicate exp) (cadr exp))
-(define (if-consequent exp) (caddr exp))
-(define (if-alternative exp)
-  (if (not (null? (cdddr exp)))
-      (cadddr exp)
-      'false))
-
 (define (eval-if exp env)
   (if (true? (eval-expr (if-predicate exp) env))
       (eval-expr (if-consequent exp) env)
       (eval-expr (if-alternative exp) env)))
-
-;;; Evaluator
 
 (define (eval-expr exp env)
   (cond ((self-evaluating? exp) exp)
@@ -57,29 +85,63 @@
         ;;;  (eval-sequence 
         ;;;   (begin-actions exp) 
         ;;;   env))
-        ;;; ((cond? exp) (eval (cond->if exp) env))
+        ;;; ((cond? exp) (eval-expr (cond->if exp) env))
         ;;; ((application? exp)
-        ;;;  (apply (eval (operator exp) env)
-        ;;;         (list-of-values 
-        ;;;          (operands exp) 
-        ;;;          env)))
+        ;;;  (apply-proc (eval-expr (operator exp) env)
+        ;;;              (list-of-values (operands exp)
+        ;;;                              env)))
         (else
-         (error "Unknown expression 
-                 type: EVAL" exp))))
+         (error "Unknown expression type: EVAL" exp))))
 
-;;; Tests
+;;; Apply ---------------------------------------------------
 
-(define (test exp expected)
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (apply-primitive-procedure proc args)
+  (apply (primitive-implementation proc) args))
+
+(define (apply-proc procedure args)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure args))
+        ;;; ((compound-procedure? procedure)
+        ;;;  (eval-sequence
+        ;;;    (procedure-body procedure)
+        ;;;    (extend-environment
+        ;;;      (procedure-parameters procedure)
+        ;;;      args
+        ;;;      (procedure-environment procedure))))
+        (else
+         (error "Unknown procedure type: APPLY"
+                procedure))))
+
+;;; Tests ---------------------------------------------------
+
+(define (test-eval exp expected)
   (define result (eval-expr exp null))
-  (if (eq? result expected)
-      (displayln "Passed")
+  (if (equal? result expected)
+      (printf "Passed: ~s~n" exp)
       (begin 
         (printf "Failed: ~s~n" exp)
         (printf "  Expected: ~s~n" expected)
-        (printf "  Actual: ~s~n" result))))
+        (printf "  Actual:   ~s~n" result))))
 
-(test '1 1)
-(test '"abc" "abc")
-(test '(if #t "true" "false") "true")
-(test '(if #f "true" "false") "false")
-(test '(if 1 "true" "false") "true")
+(define (test-apply proc args expected)
+  (define result (apply-proc proc args))
+  (if (equal? result expected)
+      (printf "Passed: (apply-proc ~s ~s)~n" proc args)
+      (begin
+        (printf "Failed: (apply-proc ~s ~s)~n" proc args)
+        (printf "  Expected: ~s~n" expected)
+        (printf "  Actual:   ~s~n" result))))
+
+(printf "Eval Tests -----------------------------------~n")
+(test-eval '1 1)
+(test-eval '"abc" "abc")
+(test-eval '(if #t "true" "false") "true")
+(test-eval '(if #f "true" "false") "false")
+(test-eval '(if 1 "true" "false") "true")
+
+(printf "~nApply Tests ----------------------------------~n")
+(define cons-proc (caddr (primitive-procedure-objects)))
+(test-apply cons-proc '(1 2) (cons 1 2))
